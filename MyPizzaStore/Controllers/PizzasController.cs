42560779496cefaca4672f6986bfa8f47e29519f
Aleks Quiñones <kigu@monobits.mx>
2021-08-20 17:28:29 -0700

    Add project files.

---
+++ MyPizzaStore/Controllers/PizzasController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPizzaStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPizzaStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Pizza>> Get() => PizzaService.GetAll();
+
+        // obtener por ID
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // guardar (POST)
+
+
+        // editar (PUT)
+
+
+        // eliminar (DELETE)
+    }
+}
---
+++ MyPizzaStore/PizzaService.cs
@@ -0,0 +1,52 @@
+﻿using MyPizzaStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPizzaStore
+{
+    // Como es una clase estática no necesita ser inicializado por lo tanto lo podemos llamar directamente
+    public static class PizzaService
+    {
+        static List<Pizza> Pizzas { get; }
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas = new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
+                new Pizza { Id = 2, Name = "Veggie", IsGlutenFree = true }
+            };
+        }
+
+        // Como es estático no necesitamos tener una instancia de PizzaService para invocarlo
+        public static List<Pizza> GetAll() => Pizzas;
+
+        public static Pizza Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+        public static void Delete(int id)
+        {
+            var pizza = Get(id);
+            if (pizza is null)
+                return;
+
+            Pizzas.Remove(pizza);
+        }
+
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if (index == -1)
+                return;
+
+            Pizzas[index] = pizza;
+        }
+    }
+}
